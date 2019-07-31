6c3a360cc557b34d7d5c7580e3fb4cbdb4a32008
davidcrowe <dcrowe@chicagobooth.edu>
2019-07-30 17:59:00 -0700

    Adding photo upload functionality via routes.py functions & upload_image.html

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_heroku import Heroku
 import psycopg2
+import os
+from werkzeug.utils import secure_filename
 from models import *
 
 app = Flask(__name__)
@@ -9,6 +11,11 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:########@localhost/housingapp'
 #heroku = Heroku(app)
+
+# Set configurations for image upload functionality
+app.config["IMAGE_UPLOADS"] = "/Users/davidcrowe/code/harvard/csci_s14a/final_project/repo/HES-CSCI_S14A-CNN_Semantic_Segmentation_Team_10/uploads"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
+app.config["MAX_CONTENT_LENGTH"] = 50 * 1024 * 1024
 
 db.init_app(app)
 
@@ -54,6 +61,43 @@ def property_detail():
 @app.route("/view-properties")
 def view_properties():
 	return render_template("view-properties.html")
+
+
+def allowed_image(filename):
+	# We only want files with a . in the filename
+	if not "." in filename:
+		return False 
+
+	# Split the extension from the filename
+	ext = filename.rsplit(".", 1)[1]
+
+	# Check if the extension is in ALLOWED_IMAGE_EXTENSIONS
+	if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+		return True
+	else:
+		return False
+
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+	if request.method == "POST":
+		if request.files:
+			image = request.files["image"]
+
+			if image.filename == "":
+				print("No filename")
+				return redirect(request.url)
+
+			if allowed_image(image.filename):
+				filename = secure_filename(image.filename)
+				image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+				print("Image saved")
+				return redirect(request.url)
+
+			else:
+				print("That file extension is not allowed")
+				return redirect(request.url)
+	return render_template("upload_image.html")
 
 
 if __name__ == "__main__":
